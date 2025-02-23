139b74fd457eecfa3a18b2f91497a2741c94a692
Fernanda4356 <20233tn129@gmail.com>
2025-02-22 22:44:32 -0600

    TAREA

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const personasRoutes = require('./routes/persona.routes');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
+app.use('/api/personas', personasRoutes);
+
+
+
+mongoose.connect('mongodb+srv://20233tn129:ali1105m.@ali.n62y5.mongodb.net/?retryWrites=true&w=majority&appName=Ali')
+    .then(() => {
+        console.log('Connected to the database!');
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log('Cannot connect to the database!', err);
+    });
